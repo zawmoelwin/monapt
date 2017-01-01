cb0a026cb4d4c1153c4cea41be8312f2b6a7f090
Kevin Li <jiawei.h.li@gmail.com>
2016-12-31 17:24:33 -0800

    build(webpack): support typescript compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: './dist'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      './src'
+    ],
+    extensions: ['.ts', '.js']
+  },
 };
